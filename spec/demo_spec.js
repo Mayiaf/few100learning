058ab980d617c9c632c208d1c9bd5dba08e2c2b6
Mariela Andrade <mariela_andrade@progressive.com>
2019-02-19 14:56:41 +0000

    initial

---
+++ spec/demo_spec.js
@@ -0,0 +1,13 @@
+import { add } from './utils';
+describe('some things', () => {
+
+    it('should behave...', () => {
+        expect(2 + 2).toBe(4);
+    });
+    it('can add DELETE ME', () => {
+        expect(add(2, 2)).toBe(4);
+    });
+    it('can add two number ', () => {
+        expect(add(2, 2)).toBe(12);
+    });
+}); 
